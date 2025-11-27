b405376bac0b5f6ca696254983bdef0d266df115
SwapyDev <sabraham2342@gmail.com>
2025-11-27 13:16:20 +0900

    Fixed dataset loader

--- src/data_processing/dataset.py
+++ src/data_processing/dataset.py
@@ -4,39 +4,39 @@ from torch_geometric.utils import degree
 class DataLoader:
     def __init__(self, root = 'data/PPI'):
         self.root = root
-        self.train = None
-        self.val = None
-        self.test = None
+        self._train = None
+        self._val = None
+        self._test = None
 
     def load(self, verbose = False):
         #Load all the splits
-        if self.train is None:
-            self.train = PPI(root=self.root, split='train')
-            self.val  = PPI(root=self.root, split='val')
-            self.test  = PPI(root=self.root, split='test')
+        if self._train is None:
+            self._train = PPI(root=self.root, split='train')
+            self._val  = PPI(root=self.root, split='val')
+            self._test  = PPI(root=self.root, split='test')
         
             if verbose:
                 self.print_info()
 
-        return self.train, self.val, self.test
+        return self._train, self._val, self._test
     
     @property
     def train(self):
-        if self.train is None:
+        if self._train is None:
             self.load()
-        return self.train 
+        return self._train 
     
     @property
     def val(self):
-        if self.val is None:
+        if self._val is None:
             self.load()
-        return self.val
+        return self._val
     
     @property
     def test(self):
-        if self.tests is None:
+        if self._test is None:
             self.load()
-        return self.test
+        return self._test
 
     def print_info(self):
         print("PPI Dataset Info")
@@ -44,6 +44,5 @@ class DataLoader:
         print(f"Val graphs:   {len(self.val)}")
         print(f"Test graphs:  {len(self.test)}")
     
-        
-
-DataLoader = DataLoader()
+ppiDataLoader = DataLoader()
+ppiDataLoader.load(verbose=True)
